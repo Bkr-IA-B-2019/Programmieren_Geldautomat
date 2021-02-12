8f7a2cc7b9481a66e23b8caef84854dd765a6578
Noah Albers <albers-noah@pm.me>
2021-02-12 16:11:10 +0100

    Datenbanksystem und grundlegende Querys hinzugefügt

---
+++ database/IDatabase.cs
@@ -0,0 +1,72 @@
+﻿using Geldautomat.database.objects;
+
+namespace Geldautomat.database
+{
+    interface IDatabase
+    {
+
+        /// <summary>
+        /// Connects the to databse
+        /// </summary>
+        /// <exception cref="DatabaseConnectionException">
+        /// Gets thrown, when the database failes to connect or open.
+        /// Contains the message of the error
+        /// </exception>
+        void Connect();
+
+        /// <summary>
+        /// Closes the connection to the database
+        /// </summary>
+        void Close();
+
+
+        /// <summary>
+        /// Searches the account that belongs to the given id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <exception cref="DatabaseException">
+        /// Gets thrown, when the successful communication with the database failes
+        /// </exception>
+        /// <returns>If the account got found, the account; otherwise null</returns>
+        Account GetAccountByID(int id);
+
+        /// <summary>
+        /// Searches all transaction belonging to the given account
+        /// </summary>
+        /// <param name="account">The account of which the transactions should be requested</param>
+        /// <exception cref="DatabaseException">
+        /// Gets thrown, when the successful communication with the database failes
+        /// </exception>
+        /// <returns>An array of all transactions a user has done</returns>
+        Transaction[] GetAccountTransactions(Account account);
+
+        /// <summary>
+        /// Inserts the new account into the database
+        /// </summary>
+        /// <param name="pseudoAccount">The account that should be inserted (Does not contain an id)</param>
+        /// <exception cref="DatabaseException">
+        /// Gets thrown, when the successful communication with the database failes
+        /// </exception>
+        /// <returns>The newly created account with the updates like the id</returns>
+        Account CreateNewAccount(Account pseudoAccount);
+
+        /// <summary>
+        /// Inserts a new transaction into the database
+        /// </summary>
+        /// <param name="transaction">The new transaction</param>
+        /// <exception cref="DatabaseException">
+        /// Gets thrown, when the successful communication with the database failes
+        /// </exception>
+        /// <returns>The created transaction with the updates from the database like the id</returns>
+        Transaction CreateNewTransaction(Transaction transaction);
+
+        /// <summary>
+        /// Updates the accounts data to the database.
+        /// </summary>
+        /// <param name="account">The account that shall be updated</param>
+        /// <exception cref="DatabaseException">
+        /// Gets thrown, when the successful communication with the database failes
+        /// </exception>
+        void UpdateAccount(Account account);
+    }
+}
---
+++ database/SqliteDatabase.cs
@@ -0,0 +1,246 @@
+﻿using Geldautomat.database.exceptions;
+using Geldautomat.database.objects;
+using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Geldautomat.database
+{
+    class SqliteDatabase : IDatabase
+    {
+        
+        /// <summary>
+        /// The connection to the database
+        /// </summary>
+        private SqliteConnection connection;
+
+        /// <summary>
+        /// The name of the open database file
+        /// </summary>
+        public string File { get; private set; }
+        public SqliteDatabase(string file)
+        {
+            this.File = file;
+            this.connection = new SqliteConnection($"data source=\"{file}\";");
+        }
+
+        public void Connect()
+        {
+            try
+            {
+                this.connection.Open();
+            }
+            catch (SqliteException)
+            {
+                throw new DatabaseConnectionException("Fehler beim öffnen der Datenbankverbindung.");
+            }
+        }
+
+        public void Close()
+        {
+            // Checks if the connection is open
+            if (this.connection.State == ConnectionState.Open)
+                // Closes the connection
+                this.connection.Close();
+        }
+
+        public Account GetAccountByID(int id)
+        {
+            try
+            {
+                // Creates a new command
+                var cmd = this.connection.CreateCommand();
+                cmd.CommandText = @"
+                    SELECT *
+                    FROM Accounts
+                    WHERE id=$id;
+                ";
+
+                // Appends the parameters
+                cmd.Parameters.AddWithValue("$id", id);
+
+                // Executes the command get gets the result
+                using (var reader = cmd.ExecuteReader())
+                    // Checks if a user got found
+                    if (reader.Read())
+                    {
+                        // Creates the new account
+                        return new Account(
+                            id,                     // Account id
+                            reader.GetDateTime(1)   // Creation date
+                        )
+                        {
+                            Firstname = reader.GetString(2),
+                            Lastname = reader.GetString(3),
+                            Money = reader.GetDecimal(4),
+                            PinHash = reader.GetString(5)
+                        };
+                    }
+                    else
+                        return null;
+            }
+            catch (SqliteException)
+            {
+                throw new DatabaseException("Fehler bei der Abfrage mit der Datenbank.");
+            }
+        }
+
+        public Transaction[] GetAccountTransactions(Account account)
+        {
+            try
+            {
+                // Creates a new command
+                var cmd = this.connection.CreateCommand();
+                cmd.CommandText = @"
+                    SELECT *
+                    FROM Transactions
+                    WHERE id=$id;
+                ";
+
+                // Appends the parameters
+                cmd.Parameters.AddWithValue("$id", account.Id);
+
+                // List with all transactions
+                var transactions = new List<Transaction>();
+
+                // Executes the command get gets the result
+                using (var reader = cmd.ExecuteReader())
+                    // Checks if a user got found
+                    while (reader.Read())
+                    {
+                        // Creates the transaction
+                        transactions.Add(
+                            new Transaction(
+                                reader.GetInt32(0),     // Id
+                                reader.GetDecimal(1),   // Money
+                                reader.GetBoolean(2),   // Subtracted
+                                account.Id,             // Account id
+                                reader.GetDateTime(3)   // Datetime
+                            )
+                        );
+                    }
+
+                // Returns all found transactions
+                return transactions.ToArray();
+            }
+            catch (SqliteException)
+            {
+                throw new DatabaseException("Fehler bei der Abfrage mit der Datenbank.");
+            }
+        }
+
+        public Account CreateNewAccount(Account pseudoAccount)
+        {
+            try
+            {
+                // Creates a new command
+                var cmd = this.connection.CreateCommand();
+                cmd.CommandText = @"
+                    INSERT INTO Accounts
+                    (createdatetime,firstname,lastname,money,pinhash)
+                    VALUES
+                    ($cdt,$fn,$ln,$m,$ph);
+                ";
+
+                // Appends the parameters
+                cmd.Parameters.AddWithValue("$cdt", pseudoAccount.CreateDateTime);
+                cmd.Parameters.AddWithValue("$fn", pseudoAccount.Firstname);
+                cmd.Parameters.AddWithValue("$ln", pseudoAccount.Lastname);
+                cmd.Parameters.AddWithValue("$m", pseudoAccount.Money);
+                cmd.Parameters.AddWithValue("$ph", pseudoAccount.PinHash);
+
+                // Executes the command get gets the result
+                cmd.ExecuteNonQuery();
+
+                // Gets the last insert id
+                int id = this.GetLastInsertedID();
+
+                // Creates the new account
+                return new Account(id,pseudoAccount);
+            }
+            catch (SqliteException)
+            {
+                throw new DatabaseException("Fehler beim sichern des Account's in der Datenbank.");
+            }
+        }
+
+        public Transaction CreateNewTransaction(Transaction pseudoTransaction)
+        {
+            try
+            {
+                // Creates a new command
+                var cmd = this.connection.CreateCommand();
+                cmd.CommandText = @"
+                    INSERT INTO Transactions
+                    (translatedmoney,subtracted,accountid,datetime)
+                    VALUES
+                    ($tm,$s,$aid,$dt);
+                ";
+
+                // Appends the parameters
+                cmd.Parameters.AddWithValue("$tm", pseudoTransaction.TranslatedMoney);
+                cmd.Parameters.AddWithValue("$s", pseudoTransaction.Subtracted);
+                cmd.Parameters.AddWithValue("$aid", pseudoTransaction.AccountId);
+                cmd.Parameters.AddWithValue("$dt", pseudoTransaction.DateTime);
+
+                // Executes the command get gets the result
+                cmd.ExecuteNonQuery();
+
+                // Gets the last insert id
+                int id = this.GetLastInsertedID();
+
+                // Creates the new account
+                return new Transaction(id, pseudoTransaction);
+            }
+            catch (SqliteException)
+            {
+                throw new DatabaseException("Fehler beim sichern der Transaktion in der Datenbank.");
+            }
+        }
+
+        public void UpdateAccount(Account account)
+        {
+            try
+            {
+                // Creates a new command
+                var cmd = this.connection.CreateCommand();
+                cmd.CommandText = @"
+                    UPDATE ACCOUNTS
+                    SET createdatetime=$cdt, firstname=$fn, lastname=$ln, money=$m, pinhash=$ph
+                    WHERE id=$id;
+                ";
+
+                // Appends the parameters
+                cmd.Parameters.AddWithValue("$cdt", account.CreateDateTime);
+                cmd.Parameters.AddWithValue("$fn", account.Firstname);
+                cmd.Parameters.AddWithValue("$ln", account.Lastname);
+                cmd.Parameters.AddWithValue("$m", account.Money);
+                cmd.Parameters.AddWithValue("$ph", account.PinHash);
+                cmd.Parameters.AddWithValue("$id", account.Id);
+
+                // Executes the command get gets the result
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqliteException)
+            {
+                throw new DatabaseException("Fehler beim sichern des Accounts in der Datenbank.");
+            }
+        }
+
+
+        /// <summary>
+        /// Gets the last inserted row_id
+        /// </summary>
+        private int GetLastInsertedID()
+        {
+            // Creates the command
+            var cmd = this.connection.CreateCommand();
+            cmd.CommandText = @"
+                SELECT last_insert_rowid();
+            ";
+            // Returns the last row id
+            return (int)((long)cmd.ExecuteScalar());
+        }
+    }
+}
