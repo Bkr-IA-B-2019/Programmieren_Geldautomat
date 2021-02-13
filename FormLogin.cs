7cd2360c440f0ac4bcadf8ad3fe87c2c66b9572c
Noah Albers <albers-noah@pm.me>
2021-02-13 17:52:50 +0100

    Erstes design des Login-Forms

---
+++ FormLogin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Geldautomat
+{
+    public partial class FormLogin : System.Windows.Forms.Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+
+            this.TopMost = true;
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+            new Thread((o) =>
+            {
+                Application.Run(new FormCreateAccount());
+            }).Start();
+        }
+    }
+}
