0565e3d99b6e145acc0765c6200ac782561467aa
Noah Albers <albers-noah@pm.me>
2021-02-14 17:28:29 +0100

    Zweites Design des Loginforms

---
+++ Forms/FormLogin.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Geldautomat
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.labelId = new System.Windows.Forms.Label();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.toolbar = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBoxPin = new System.Windows.Forms.TextBox();
+            this.labelPin = new System.Windows.Forms.Label();
+            this.buttonRegister = new System.Windows.Forms.Button();
+            this.toolbar.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(75)))), ((int)(((byte)(75)))));
+            this.textBoxId.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxId.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBoxId.Font = new System.Drawing.Font("Rockwell Nova", 13.77391F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxId.ForeColor = System.Drawing.Color.White;
+            this.textBoxId.Location = new System.Drawing.Point(0, 33);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.Size = new System.Drawing.Size(223, 36);
+            this.textBoxId.TabIndex = 0;
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelTitle.BackColor = System.Drawing.Color.Transparent;
+            this.labelTitle.Font = new System.Drawing.Font("Rockwell Nova", 16.27826F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.ForeColor = System.Drawing.Color.White;
+            this.labelTitle.Location = new System.Drawing.Point(0, 0);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(223, 70);
+            this.labelTitle.TabIndex = 1;
+            this.labelTitle.Text = "Anmeldung";
+            this.labelTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelTitle.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Toolbar_OnMouseDown);
+            this.labelTitle.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Toolbar_OnMouseMove);
+            this.labelTitle.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Toolbar_OnMouseUp);
+            // 
+            // labelId
+            // 
+            this.labelId.Dock = System.Windows.Forms.DockStyle.Top;
+            this.labelId.Font = new System.Drawing.Font("Tahoma", 13.77391F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelId.ForeColor = System.Drawing.Color.White;
+            this.labelId.Location = new System.Drawing.Point(0, 0);
+            this.labelId.Name = "labelId";
+            this.labelId.Padding = new System.Windows.Forms.Padding(20, 0, 20, 0);
+            this.labelId.Size = new System.Drawing.Size(223, 33);
+            this.labelId.TabIndex = 2;
+            this.labelId.Text = "ID";
+            this.labelId.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.buttonLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(137)))), ((int)(((byte)(137)))), ((int)(((byte)(137)))));
+            this.buttonLogin.FlatAppearance.BorderSize = 0;
+            this.buttonLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonLogin.Font = new System.Drawing.Font("Consolas", 13.77391F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLogin.ForeColor = System.Drawing.Color.White;
+            this.buttonLogin.Location = new System.Drawing.Point(61, 260);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(129, 37);
+            this.buttonLogin.TabIndex = 5;
+            this.buttonLogin.Text = "Anmelden";
+            this.buttonLogin.UseVisualStyleBackColor = false;
+            // 
+            // toolbar
+            // 
+            this.toolbar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(109)))), ((int)(((byte)(255)))));
+            this.toolbar.Controls.Add(this.panel3);
+            this.toolbar.Controls.Add(this.labelTitle);
+            this.toolbar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.toolbar.Location = new System.Drawing.Point(0, 0);
+            this.toolbar.Name = "toolbar";
+            this.toolbar.Size = new System.Drawing.Size(256, 70);
+            this.toolbar.TabIndex = 6;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.pictureBox1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel3.Location = new System.Drawing.Point(229, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(27, 70);
+            this.panel3.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::Geldautomat.Properties.Resources.Close;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(27, 70);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.Controls.Add(this.textBoxId);
+            this.panel1.Controls.Add(this.labelId);
+            this.panel1.Location = new System.Drawing.Point(12, 76);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(223, 69);
+            this.panel1.TabIndex = 7;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel2.Controls.Add(this.textBoxPin);
+            this.panel2.Controls.Add(this.labelPin);
+            this.panel2.Location = new System.Drawing.Point(12, 167);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(223, 69);
+            this.panel2.TabIndex = 8;
+            // 
+            // textBoxPin
+            // 
+            this.textBoxPin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(75)))), ((int)(((byte)(75)))));
+            this.textBoxPin.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxPin.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBoxPin.Font = new System.Drawing.Font("Rockwell Nova", 13.77391F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPin.ForeColor = System.Drawing.Color.White;
+            this.textBoxPin.Location = new System.Drawing.Point(0, 33);
+            this.textBoxPin.Name = "textBoxPin";
+            this.textBoxPin.Size = new System.Drawing.Size(223, 36);
+            this.textBoxPin.TabIndex = 0;
+            // 
+            // labelPin
+            // 
+            this.labelPin.Dock = System.Windows.Forms.DockStyle.Top;
+            this.labelPin.Font = new System.Drawing.Font("Tahoma", 13.77391F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPin.ForeColor = System.Drawing.Color.White;
+            this.labelPin.Location = new System.Drawing.Point(0, 0);
+            this.labelPin.Name = "labelPin";
+            this.labelPin.Padding = new System.Windows.Forms.Padding(20, 0, 20, 0);
+            this.labelPin.Size = new System.Drawing.Size(223, 33);
+            this.labelPin.TabIndex = 2;
+            this.labelPin.Text = "Pin";
+            this.labelPin.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // buttonRegister
+            // 
+            this.buttonRegister.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.buttonRegister.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(137)))), ((int)(((byte)(137)))), ((int)(((byte)(137)))));
+            this.buttonRegister.FlatAppearance.BorderSize = 0;
+            this.buttonRegister.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonRegister.Font = new System.Drawing.Font("Consolas", 13.77391F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRegister.ForeColor = System.Drawing.Color.White;
+            this.buttonRegister.Location = new System.Drawing.Point(43, 313);
+            this.buttonRegister.Name = "buttonRegister";
+            this.buttonRegister.Size = new System.Drawing.Size(168, 37);
+            this.buttonRegister.TabIndex = 9;
+            this.buttonRegister.Text = "Registrieren";
+            this.buttonRegister.UseVisualStyleBackColor = false;
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(54)))), ((int)(((byte)(54)))));
+            this.ClientSize = new System.Drawing.Size(256, 362);
+            this.ControlBox = false;
+            this.Controls.Add(this.buttonRegister);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.toolbar);
+            this.Controls.Add(this.buttonLogin);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.MaximumSize = new System.Drawing.Size(420, 420);
+            this.MinimumSize = new System.Drawing.Size(230, 380);
+            this.Name = "FormLogin";
+            this.toolbar.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Label labelId;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Panel toolbar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBoxPin;
+        private System.Windows.Forms.Label labelPin;
+        private System.Windows.Forms.Button buttonRegister;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Forms/FormLogin.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Geldautomat
+{
+    public partial class FormLogin : System.Windows.Forms.Form
+    {
+        // If the mouse got pressed on the toolbar to drag
+        private bool windowDragged = false;
+
+        // At wich coordinates the mouse got clicked
+        private int dragX, dragY;
+
+        public FormLogin()
+        {
+            InitializeComponent();
+
+            this.TopMost = true;
+        }
+
+        /// <summary>
+        /// Eventhanlder for when the mouse gets pressed on the toolbar
+        /// </summary>
+        private void Toolbar_OnMouseDown(object sender, MouseEventArgs e)
+        {
+            this.windowDragged = true;
+            this.dragX = e.X+9;
+            this.dragY = e.Y+9;
+        }
+
+        private void Toolbar_OnMouseUp(object sender, MouseEventArgs e)
+        {
+            this.windowDragged = false;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Toolbar_OnMouseMove(object sender, MouseEventArgs e)
+        {
+            // Checks if the mouse is pressed
+            if(this.windowDragged)
+                // Updates the position
+                this.SetDesktopLocation(MousePosition.X - this.dragX,MousePosition.Y - this.dragY);
+        }
+    }
+}
