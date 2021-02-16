0de6e1c494f61f2a1e5a20ee0384a89ac318cde6
Noah Albers <albers-noah@pm.me>
2021-02-16 11:48:48 +0100

    Zwei Dateien ausgetauscht. Wurden bei den Restlichen Commits nicht übertragen

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Geldautomat.Forms;
+using System;
 using System.Windows.Forms;
 
 namespace Geldautomat
@@ -16,7 +14,7 @@ namespace Geldautomat
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormLogin());
         }
     }
 }
